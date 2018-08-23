8a5d5fb19bb14a1c16614fc5dff4f5bd3fd866c0
venson <vegasman23@gmail.com>
2018-08-23 18:37:05 -0500

    Updated gitignore for vscode

---
+++ Hello World!/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hello_World_
+{ /*this class is pretty dope */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! this is c#");
+
+            // this is your father
+
+            /*this
+            is
+            just
+            a 
+            test to 
+            see
+            what this multi line thingy is */
+        }
+    }
+}
+ 
